8aa832b54c0294d12e7a9f14037fd5f925941086
lightlu <lightlu@gmail.com>
2015-10-02 17:52:24 +0800

    update color-theme for x-window emacs

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -10,26 +10,27 @@
 ;; {{ Set background color
 (defun init-color-theme-set-bg-color (color)
   "Set background COLOR."
-  (when (not(eq color "nil"))
+  (when (not(eq color nil))
     (set-face-background 'default color nil)
+;;    (set-face-background 'default "DimGray")
     (set-background-color color)))
 
-(defcustom init-color-theme-window-system-bg-color "nil"
-  "Set background color when \"window-system\". If nil uses default background."
-  :type '(string)
-  :group 'init-color-theme
-  :set #'(lambda (sym value)
-	   (when window-system
-	     (init-color-theme-set-bg-color value)))
-  )
+;(defcustom init-color-theme-window-system-bg-color "nil"
+;  "Set background color when \"window-system\". If nil uses default background."
+;  :type '(string)
+;  :group 'init-color-theme
+;  :set #'(lambda (sym value)
+;           (when window-system
+;             (init-color-theme-set-bg-color value)))
+;  )
 
 (defcustom init-color-theme-no-window-system-bg-color "nil"
   "Set background color when not a \"window-system\". If nil uses default background."
   :type '(string)
   :group 'init-color-theme
   :set #'(lambda (sym value)
-	   (unless window-system
-	     (init-color-theme-set-bg-color value)))
+           (unless window-system
+             (init-color-theme-set-bg-color value)))
   )
 
 (provide 'init-color-theme)
